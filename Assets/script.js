b3d5e80e657e29fbf3c1f7b8c2547d85408c8787
warren clark <wclarkcito@gmail.com>
2021-01-18 14:54:53 -0800

    quiz questions are now visible when start quiz button is pressed, correct answers now register

--- Assets/script.js
+++ Assets/script.js
@@ -3,69 +3,72 @@
 //objects within array//
 var quizQuestions = [
     
-    question1 = {
+    {
         question: "Where were noodles invented?",
         possibilities: ["Italy", "Spain", "China", "Singapore"],
         correctAnswer: "China"
     },
-    question2 = {
+     {
         question: "What country has the largest population?",
         possibilities: ['USA', 'France', 'China', "Russia"],
         correctAnswer: "China"
     },
-    question3 = {
+    {
         question: "What breed of dog has the most health problems?",
-        possibilities: ["Doberman", "Golden Retriever", "cocker Spaniel", "German Shephard"],
-        correctAnswer: "c"
+        possibilities: ["Doberman", "Golden Retriever", "Cocker Spaniel", "German Shephard"],
+        correctAnswer: "Cocker Spaniel"
     },
     {
-    question4 = {
+      
         question: "Where is the hottest recorded temperature on earth?",
         possibilities: ["Dallas, Tx", "Damascus, Syria", "Amman, Jordan", "Death Valley, Ca"],
-        correctAnswer: "d"
+        correctAnswer: "Death Valley, Ca"
     },
 
-    question5: {
+     {
         question: "what organized crime group is the most powerful in the world?",
         possibilities: ["Italian Mafia", "Mexican Cartel", "Japanese Mafia/Yakuza", "Russian Mafia"],
-        correctAnswer: "d"
+        correctAnswer: "Russian Mafia"
     },
 
-    question6: {
+     {
         question: "what is the most popular cereal in the world?",
         possibilities: ["Frosted Flakes", "Honey Nut Cheerios", "Corn Flakes", "Raisin Bran" ],
-        correctAnswer: "b"
+        correctAnswer: "Honey Nut Cheerios"
     },
 
-    question7: {
+     {
         question: "what is the worlds most popular beer?",
         possibilities: ["Budweiser", "Snow", "Corona", "Tsingtao"],
-        correctAnswer: "b"
+        correctAnswer: "Snow"
     },
 
-    question8: {
+     {
         question: "What is the most profitable industry in the world?",
         possibilities: ["Pharmaceuticals", "Tobacco", "Mining, Crude-Oil Production", "Food Consumer Products"],
-        correctAnswer: "c"
+        correctAnswer: "Mining, Crude-Oil Production"
     },
 
-    question9: {
+    {
         question: "What is the most expensive city in the world to live?",
         possibilities: ["Sydney, Australia", "Hong Kong", "Los Angeles, Ca","San Francisco, Ca"],
-        correctAnswer: "b"
+        correctAnswer: "Hong Kong"
     },
 
 
-    question0: {
+     {
         question: "What was the first country in the world?",
         possibilities: ["Greece", "Japan", "Egypt", "Ethiopia"],
-        correctAnswer: "d"
+        correctAnswer: "Ethiopia"
     },
 
 
 ];
 var highScores = []
 var submitButton = document.getElementById("startQuiz");
+submitButton.addEventListener("click", e=>{
+    startQuiz()
+})
 //header
 var viewScore = document.getElementById("viewScore");
 var timer = document.getElementById("timer");
@@ -101,10 +104,28 @@ var highContainer = document.getElementById("highscore-container");
 var goBackBtn = document.getElementById("go-back");
 
 var pageArray = [mainPage, quizPage, finalScorePage, highscorePage];
+//created function to list questions and the 
+function generateQuestion(arr, index){
+    quizQuestion.innerHTML=arr[index].question;
+    quizAnswers.innerHTML="";
+    arr[index].possibilities.forEach(element => {
+    var listItem = document.createElement("li");
+    listItem.innerHTML=element;
+    listItem.addEventListener("click", e=>{
+        console.log("answer clicked")
+        
+        var point = element === arr[index].correctAnswer? 1: 0;
+        console.log(point)
+        nextQuestion(point, index +1);
+    }); 
+    quizAnswers.appendChild(listItem);  
+    });
+}
 
-submitButton.addEventListener('click', startQuiz);
-
-
+function nextQuestion(point, index){
+    score += point;
+    generateQuestion(quizQuestions, index);
+}
 function hidePages() {
     for(var i = 0; i < pageArray.length; i++) {
         if(!pageArray[i].classList.contains('hide')) {
@@ -141,6 +162,7 @@ function startQuiz() {
     questionIndex = 0;
 
     quizPage.classList.remove('hide');
+    generateQuestion(quizQuestions, 0);
 }
 
 function arrayShuffle(arr) {
