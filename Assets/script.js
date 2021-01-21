435c8c773df613551c5eddbe82253d6493720eba
warren clark <wclarkcito@gmail.com>
2021-01-20 16:24:17 -0800

    added notes to script file. Had a great deal of help with the functions from my tudor on this assignment. there are still a few errors but its the best I could do

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,15 @@
 
 
-//objects within array//
+
 var timer;
 var quizQuestions = [
-    
+
     {
         question: "Where were noodles invented?",
         possibilities: ["Italy", "Spain", "China", "Singapore"],
         correctAnswer: "China"
     },
-     {
+    {
         question: "What country has the largest population?",
         possibilities: ['USA', 'India', 'China', "Russia"],
         correctAnswer: "China"
@@ -20,31 +20,31 @@ var quizQuestions = [
         correctAnswer: "Cocker Spaniel"
     },
     {
-      
+
         question: "Where is the hottest recorded temperature on earth?",
         possibilities: ["Dallas, Tx", "Damascus, Syria", "Amman, Jordan", "Death Valley, Ca"],
         correctAnswer: "Death Valley, Ca"
     },
 
-     {
+    {
         question: "what organized crime group is the most powerful in the world?",
         possibilities: ["Italian Mafia", "Mexican Cartel", "Japanese Mafia/Yakuza", "Russian Mafia"],
         correctAnswer: "Russian Mafia"
     },
 
-     {
+    {
         question: "what is the most popular cereal in the world?",
-        possibilities: ["Frosted Flakes", "Honey Nut Cheerios", "Corn Flakes", "Raisin Bran" ],
+        possibilities: ["Frosted Flakes", "Honey Nut Cheerios", "Corn Flakes", "Raisin Bran"],
         correctAnswer: "Honey Nut Cheerios"
     },
 
-     {
+    {
         question: "what is the worlds most popular beer?",
         possibilities: ["Budweiser", "Snow", "Corona", "Tsingtao"],
         correctAnswer: "Snow"
     },
 
-     {
+    {
         question: "What is the most profitable industry in the world?",
         possibilities: ["Pharmaceuticals", "Tobacco", "Mining, Crude-Oil Production", "Food Consumer Products"],
         correctAnswer: "Mining, Crude-Oil Production"
@@ -52,12 +52,12 @@ var quizQuestions = [
 
     {
         question: "What is the most expensive city in the world to live?",
-        possibilities: ["Sydney, Australia", "Hong Kong", "Los Angeles, Ca","San Francisco, Ca"],
+        possibilities: ["Sydney, Australia", "Hong Kong", "Los Angeles, Ca", "San Francisco, Ca"],
         correctAnswer: "Hong Kong"
     },
 
 
-     {
+    {
         question: "What was the first country in the world?",
         possibilities: ["Greece", "Japan", "Egypt", "Ethiopia"],
         correctAnswer: "Ethiopia"
@@ -71,7 +71,7 @@ var quizQuestions = [
 var highScores = []
 var submitButton = document.getElementById("startQuiz");
 
-submitButton.addEventListener("click", e=>{
+submitButton.addEventListener("click", e => {
     startQuiz()
 })
 //header
@@ -90,83 +90,82 @@ var quizQuestion = document.getElementById("questions");
 var quizAnswers = document.getElementById("answers");
 // final score page
 var finalScorePage = document.getElementById("finalpage-quiz");
-/*
-create variables based of IDs 
-*/
+
 var displayScore = document.getElementById("display-score");
-var score=0;
+var score = 0;
 //highscore page
 var highscorePage = document.getElementById('highscorepage-quiz')
 
 var highContainer = document.getElementById("highscore-container");
-//var goBackBtn = document.getElementById("go-back");
+
 
 var pageArray = [mainPage, quizPage, finalScorePage, highscorePage];
 
 
 //created function to list and answer questions
-function generateQuestion(arr, index){
-    quizQuestion.innerHTML=arr[index].question;
-    quizAnswers.innerHTML="";
+function generateQuestion(arr, index) {
+    quizQuestion.innerHTML = arr[index].question;
+    quizAnswers.innerHTML = "";
     arr[index].possibilities.forEach(element => {
-    let listItem = document.createElement("li");
-    listItem.innerHTML=element;
-    listItem.addEventListener("click", e=>{
-        console.log("answer clicked")
-        
-        let point = element === arr[index].correctAnswer? 1: 0;
-        console.log(point)
-        nextQuestion(point, index +1);
-    }); 
-    quizAnswers.appendChild(listItem);  
+        let listItem = document.createElement("li");
+        listItem.innerHTML = element;
+        listItem.addEventListener("click", e => {
+            console.log("answer clicked")
+
+            let point = element === arr[index].correctAnswer ? 1 : 0;
+            console.log(point)
+            nextQuestion(point, index + 1);
+        });
+        quizAnswers.appendChild(listItem);
     });
 }
 //enumerates correct answers
-function nextQuestion(point, index){
+function nextQuestion(point, index) {
     score += point;
-    if(index === quizQuestions.length){
+    if (index === quizQuestions.length) {
         quiz = false;
         endGame()
         clearInterval(timer);
-    }else{
+    } else {
         generateQuestion(quizQuestions, index);
-        
-    }
-    
-}
 
-function endGame(){
+    }
+
+}
+//created button to start quiz over once completed
+function endGame() {
     hidePages();
     finalScorePage.classList.remove("hide")
-    displayScore.innerHTML=score;
-    let button = document.createElement("button");
+    displayScore.innerHTML = score;
+    var button = document.createElement("button");
     button.innerText = "start new quiz";
-    button.addEventListener("click", e=> startQuiz());
+    button.addEventListener("click", e => startQuiz());
     finalScorePage.appendChild(button);
 }
 function hidePages() {
-    for(var i = 0; i < pageArray.length; i++) {
-        if(!pageArray[i].classList.contains('hide')) {
+    for (var i = 0; i < pageArray.length; i++) {
+        if (!pageArray[i].classList.contains('hide')) {
             pageArray[i].classList.add('hide');
         }
     }
 }
 
 //Timer Function
-var quiz = false; 
-function startTimer(flag){
-    if(!quiz){
-    var timeLeft = 60;
-     timer = setInterval(function(){
-        timeLeft--;
-        timerDisplay.innerHTML=timeLeft;
-        if(timeLeft <= 0 ){
-            clearInterval(timer);
-            endGame()
-        }
+var quiz = false;
+function startTimer(flag) {
+    if (!quiz) {
+        var timeLeft = 60;
+        timer = setInterval(function () {
+            timeLeft--;
+            timerDisplay.innerHTML = timeLeft;
+            if (timeLeft <= 0) {
+                clearInterval(timer);
+                endGame()
+            }
 
-    }, 1000)}
-    
+        }, 1000)
+    }
+
 }
 
 var quizTime = 60;
@@ -178,12 +177,12 @@ var questionIndex;
 
 function questionOrder(arr) {
     var arrayIndex = [];
-    for(var i = 0; i < arr.length; i++) {
+    for (var i = 0; i < arr.length; i++) {
         arrayIndex.push(i);
     }
     return arrayShuffle(arrayIndex);
 }
-
+//function that starts quiz
 function startQuiz() {
     startTimer(true)
     alert("quiz started");
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Code-Quiz
 
-## This is a program that generates 10 random quiz questions with 4 possible answers.  The correct answers are stored and a high score is displayed upon completion.
+## This is a program that generates 10 random quiz questions with 4 possible answers. The correct answers are stored and a high score is displayed upon completion.
 
 ## https://github.com/wclarkcito/Code-Quiz
+
 ## https://wclarkcito.github.io/Code-Quiz/
+
+![Screen Shot 2021-01-20 at 4 06 35 PM](https://user-images.githubusercontent.com/73144564/105256400-90c51d00-5b3a-11eb-8ddc-218f979dcccc.png)
